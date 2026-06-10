0b61810c6658fdc1f71642990f1f4f1441183ab9
Akshat Patel <akshatpatel2310@gmail.com>
2026-06-10 13:41:22 +0530

    Time: 3 ms (65.79%), Space: 10 MB (70.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,8 +1,7 @@
 class Solution {
 private:
-
     bool checkequal(int a[26], int b[26]){
-        
+
         for(int i=0; i<26; i++){
             if(a[i] != b[i]){
                 return false;
@@ -13,45 +12,45 @@ private:
 public:
     bool checkInclusion(string s1, string s2) {
 
-        if( s1.length() > s2.length()){
+        if( s1.length() > s2.length() ){
             return false;
         }
 
         int countofs1[26] = {0};
-
-        for(int i=0; i<s1.length(); i++) {
+        for(int i=0; i<s1.length(); i++){
             int index;
             index = s1[i] - 'a';
             countofs1[index]++;
         }
 
-        int i = 0;
+        int i=0;
         int windowsize = s1.length();
         int countofs2[26] = {0};
 
-        while(i < windowsize && i < s2.length()){
+        while(i < windowsize){
             int index;
             index = s2[i] - 'a';
             countofs2[index]++;
             i++;
         }
 
-        if( checkequal(countofs1, countofs2)){
+        if( checkequal(countofs1,countofs2)){
             return true;
         }
 
         while(i < s2.length()){
+            int index;
             char newchar = s2[i];
-            int index = newchar - 'a';
+            index = newchar - 'a';
             countofs2[index]++;
-
+            
             char oldchar = s2[i - windowsize];
             index = oldchar - 'a';
             countofs2[index]--;
             i++;
 
             if( checkequal(countofs1, countofs2)){
-            return true;
+                return true;
             }
         }
         return false;
