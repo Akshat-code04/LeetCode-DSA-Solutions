8fa6b4eee724bfb05fe2cea66827009e35bf99d0
Akshat Patel <akshatpatel2310@gmail.com>
2026-06-10 13:49:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    // checking both the arrays are equal or not 
     bool checkequal(int a[26], int b[26]){
 
         for(int i=0; i<26; i++){
@@ -12,6 +13,7 @@ private:
 public:
     bool checkInclusion(string s1, string s2) {
 
+        // if s1 is larger than s2 then permutation is not possible 
         if( s1.length() > s2.length() ){
             return false;
         }
@@ -27,32 +29,37 @@ public:
         int windowsize = s1.length();
         int countofs2[26] = {0};
 
+        // reading the first window of s1 length
         while(i < windowsize){
             int index;
             index = s2[i] - 'a';
             countofs2[index]++;
             i++;
         }
-
+        // check array after reading the first window of size s1
         if( checkequal(countofs1,countofs2)){
             return true;
         }
-
+        // checking for next window character by character 
         while(i < s2.length()){
             int index;
+
+            // entering new character in reading window
             char newchar = s2[i];
             index = newchar - 'a';
             countofs2[index]++;
             
+            // removing the old character from reading window 
             char oldchar = s2[i - windowsize];
             index = oldchar - 'a';
             countofs2[index]--;
             i++;
 
+            // check array after entering newcharacter and removing old character 
             if( checkequal(countofs1, countofs2)){
                 return true;
             }
         }
-        return false;
+        return false; // still if we don't get our prmutation that means permutaion doesn't exist 
     }
 };
