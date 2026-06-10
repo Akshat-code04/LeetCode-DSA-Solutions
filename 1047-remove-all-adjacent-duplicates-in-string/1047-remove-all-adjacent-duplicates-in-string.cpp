759b261e89040ce346ee3861cf12002398a8306b
Akshat Patel <akshatpatel2310@gmail.com>
2026-06-10 14:11:24 +0530

    Time: 3 ms (80.76%), Space: 13.7 MB (69.56%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string ans = ""; // This string will act as our stack
-        
-        // Traverse through each character of the input string
-        for(int i = 0; i < s.length(); i++) {
-            
-            // If ans is not empty and current char matches the last char of ans
-            if(ans.length() > 0 && s[i] == ans.back()) {
-                ans.pop_back(); // Remove the duplicate adjacent pair (Pop operation)
-            } 
-            else {
-                ans.push_back(s[i]); 
+
+        string ans = "";
+
+        for(int i=0; i<s.length(); i++){
+            if(ans.length() > 0 && ans.back() == s[i]){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
             }
         }
-        
-        return ans; 
+        return ans;
     }
 };
