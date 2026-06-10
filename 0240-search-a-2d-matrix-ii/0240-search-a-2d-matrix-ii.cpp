f1e46a7afaa1d34cecad9db3973dad8766105e6c
Akshat Patel <akshatpatel2310@gmail.com>
2026-06-10 18:25:42 +0530

    Time: 58 ms (32.72%), Space: 18.6 MB (67.25%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
         int row = matrix.size();
         int col = matrix[0].size();
-        
-        // Start from the top-right corner of the matrix
-        int rowIndex = 0;
-        int colIndex = col - 1;
-        
-        // Loop runs until pointers stay inside the matrix boundaries
-        while (rowIndex < row && colIndex >= 0) {
-            int element = matrix[rowIndex][colIndex];
-            
-            // Check if current element is the target
-            if (element == target) {
-                return true; 
+
+        int rowindex = 0;
+        int colindex = col -1; // last column 
+
+        while(rowindex < row && colindex >= 0){
+            int element = matrix[rowindex][colindex];
+
+            if(element == target){
+                return true;
             }
-            
-            // If element is smaller than target, move down to the next row (larger numbers)
-            if (element < target) {
-                rowIndex++;
-            } 
-            // If element is greater than target, move left to the previous column (smaller numbers)
-            else {
-                colIndex--;
+            else if(element < target){
+                rowindex++;
+            }
+            else{
+                colindex--;
             }
         }
-        
-        return false; // Target not found after complete search
+        return false;
     }
 };
