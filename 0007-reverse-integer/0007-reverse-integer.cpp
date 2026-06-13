083ba958ad80bdb988469b13590a0aefb52c5e7f
Akshat Patel <akshatpatel2310@gmail.com>
2026-06-13 21:34:01 +0530

    Time: 5 ms (20.43%), Space: 8.7 MB (19.97%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,10 +4,11 @@ public:
         
         long long ans = 0;
         while(x!=0){
-            int last_digit = x%10;
-            ans = (ans*10) + last_digit;
-            x = x/10;
+            int last_digit = x%10; // find out last digit 
+            ans = (ans*10) + last_digit; // store ans*10 + last_digit in ans 
+            x = x/10; // remove the last digit of the number 
         }
+        // in case if ans value is out "int" range then return 0
         if(ans > INT_MAX || ans < INT_MIN){
             return 0;
         }
